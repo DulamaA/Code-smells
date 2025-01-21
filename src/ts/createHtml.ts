ba00f94acb666cb259cb4e89c0670cf7594e18ab
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-01-21 17:34:22 +0100

    (refactor): add specific parameters for each function, add class for image, add interface file

--- src/ts/createHtml.ts
+++ src/ts/createHtml.ts
@@ -1,84 +1,82 @@
 import { getPodcasts } from "./api";
+import { IPodcast, IPodcastsResponse } from "./interfaces";
 
 const podCastContainer = document.querySelector(
   ".section__podlist-pods",
 ) as HTMLElement;
 
-if (!podCastContainer) {
-  // eslint-disable-next-line no-console
-  console.error("Container element not found!");
-  throw new Error("Unable to find container for podcasts.");
-}
-
-interface IPodcast {
-  programurl: string;
-  socialimage: string;
-  description: string;
-  name: string;
-}
-
-interface IPodcastsResponse {
-  programs: IPodcast[];
-}
-
-export async function createHtml(): Promise<void> {
+export async function createHtml() {
   const podCasts: IPodcastsResponse = await getPodcasts();
 
-  podCasts.programs.forEach((podcast) => {
+  podCasts.programs.forEach((podcast: IPodcast) => {
     const innerArticle = createInnerArticle();
     const textDiv = createTextDiv(innerArticle);
+    const img = createImage(
+      podcast.socialimage,
+      `Bild för podcasten ${podcast.name}`,
+    );
+    innerArticle.appendChild(img);
 
-    createImg(podcast, innerArticle);
-    createHeader(podcast, textDiv);
-    createP(podcast, textDiv);
-    createLink(podcast, textDiv);
-
-    function createInnerArticle(): HTMLElement {
-      const innerArticle = document.createElement("article");
-      innerArticle.setAttribute("class", "section__article-innerarticle");
-      innerArticle.setAttribute("tabindex", "1");
-      podCastContainer.appendChild(innerArticle);
-      return innerArticle;
-    }
-
-    function createTextDiv(parent: HTMLElement): HTMLElement {
-      const textDiv = document.createElement("div");
-      textDiv.setAttribute("class", "section__article-div");
-      parent.appendChild(textDiv);
-      return textDiv;
-    }
-
-    function createLink(podcast: IPodcast, parent: HTMLElement): void {
-      const linkPlacement = document.createElement("a");
-      const linkText = document.createTextNode("Lyssna här");
-      linkPlacement.setAttribute("href", podcast.programurl);
-      linkPlacement.setAttribute("tabindex", "1");
-      linkPlacement.appendChild(linkText);
-      parent.appendChild(linkPlacement);
-    }
-    function createImg(podcast: IPodcast, parent: HTMLElement): void {
-      const imgPlacement = document.createElement("img");
-      imgPlacement.setAttribute("src", podcast.socialimage);
-      imgPlacement.setAttribute("alt", podcast.name || "Podcast-bild");
-      imgPlacement.setAttribute("width", "100");
-      imgPlacement.setAttribute("height", "100");
-      parent.appendChild(imgPlacement);
-    }
-
-    function createP(podcast: IPodcast, parent: HTMLElement): void {
-      const descPlacement = document.createElement("p");
-      const desc = document.createTextNode(podcast.description);
-      descPlacement.appendChild(desc);
-      parent.appendChild(descPlacement);
-    }
-
-    function createHeader(podcast: IPodcast, parent: HTMLElement): void {
-      const headerPlacement = document.createElement("h2");
-      const programName = document.createTextNode(podcast.name);
-      headerPlacement.appendChild(programName);
-      parent.appendChild(headerPlacement);
-    }
+    createHeader(textDiv, podcast.name);
+    createP(textDiv, podcast.description);
+    createLink(textDiv, podcast.programurl);
   });
+}
+
+function createInnerArticle(): HTMLElement {
+  const innerArticle = document.createElement("article");
+  innerArticle.setAttribute("class", "section__article-innerarticle");
+  innerArticle.setAttribute("tabindex", "1");
+  podCastContainer.appendChild(innerArticle);
+  return innerArticle;
+}
+
+function createTextDiv(innerArticle: HTMLElement): HTMLElement {
+  const textDiv = document.createElement("div");
+  textDiv.setAttribute("class", "section__article-div");
+  innerArticle.appendChild(textDiv);
+  return textDiv;
+}
+
+function createLink(textDiv: HTMLElement, programUrl: string): void {
+  const linkPlacement = document.createElement("a");
+  const linkText = document.createTextNode("Lyssna här");
+  linkPlacement.setAttribute("href", programUrl);
+  linkPlacement.setAttribute("tabindex", "1");
+  linkPlacement.appendChild(linkText);
+  textDiv.appendChild(linkPlacement);
+}
+
+function createImage(
+  src: string,
+  alt: string = "",
+  className: string = "podcast-image",
+  width: number = 100,
+  height: number = 100,
+): HTMLImageElement {
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = alt;
+  img.width = width;
+  img.height = height;
+
+  img.classList.add(className);
+
+  return img;
+}
+
+function createP(textDiv: HTMLElement, description: string): void {
+  const descPlacement = document.createElement("p");
+  const desc = document.createTextNode(description);
+  descPlacement.appendChild(desc);
+  textDiv.appendChild(descPlacement);
+}
+
+function createHeader(textDiv: HTMLElement, programName: string): void {
+  const headerPlacement = document.createElement("h2");
+  const programNameText = document.createTextNode(programName);
+  headerPlacement.appendChild(programNameText);
+  textDiv.appendChild(headerPlacement);
 }
 
 export default createHtml;
