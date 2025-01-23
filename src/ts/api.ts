7d9d297d6f1bc79757231fff50c2f842b23abc3d
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-01-23 12:40:40 +0100

    (fix): update tsconfig.json with vite to read the .env file

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -1,28 +1,23 @@
-import dotenv from "dotenv";
-
-//------ Ladda miljövariabler från .env-filen---------
-dotenv.config();
-
 //------Hämta API-adressen från miljövariabeln--------
-const PODCAST_API_URL = process.env.PODCAST_API_URL;
+const PODCAST_API_URL = import.meta.env.VITE_PODCAST_API_URL;
 
 //------Kontrollera om variabeln är definierad--------
 if (!PODCAST_API_URL) {
-  throw new Error("PODCAST_API_URL är inte definierad i .env-filen");
+  throw new Error("vITE_PODCAST_API_URL är inte definierad i .env-filen");
 }
 
 //------Hämtar podcast-data från API:et---------------
 export async function getPodcasts() {
-  return await fetch(
-    "https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false",
-  )
-    .then((data) => data.json())
-    .then((json) => json)
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.error("Fel vid hämtning av poddar:", error);
-      return null;
-    });
+  try {
+    const response = await fetch(PODCAST_API_URL);
+    if (!response.ok) {
+      throw new Error(`API-anropet misslyckades: ${response.statusText}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Fel vid hämtning av poddar:", error);
+    return null;
+  }
 }
 
 export default getPodcasts;
